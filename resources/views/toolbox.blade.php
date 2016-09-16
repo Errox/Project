b62fe405438243275552ba4871ca24939cdae4eb
Errox <ryangroenewold@hotmail.com>
2016-09-16 11:30:14 +0200

    CSS huge update.

--- resources/views/toolbox.blade.php
+++ resources/views/toolbox.blade.php
@@ -6,27 +6,49 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Toolbox</div>
-
                 <div class="panel-body">
-                  <?php foreach ($toolbox_chapters as $chapters){ ?>
-
-                  <h2><?=$chapters->chapter?></h2>
-                      <form method='POST' name="toolboxform" action='/toolbox'>
+                  @foreach($toolbox_chapters as $chapters)
+                    <h2 style="text-align:center;">{{$chapters->chapter}}</h2>
+                    <form method='POST' name="toolboxform" action='/toolbox'>
                       {!! csrf_field() !!}
-                       <?php foreach ($toolbox_questions as $questions){
-                              if ($chapters->id == $questions->toolbox_chapter_id){?>
-                               <label><?=$questions->question?>:</label>
-                               <input type='text' name='<?=$questions->question?>'<?=$disable?>>
-                               <br>
-                    <?php  }
+                      @foreach ($toolbox_questions as $question)
+                        @if($chapters->id == $question->toolbox_chapter_id)
+                          <div class="row">
+                            <div class="span4 collapse-group">
+                              <p><a style="float:right;" class="btn" href="#">Lees meer &raquo;</a></p>
+                              <label class="control-label">{{$question->question}}:</label>
+                               <p class="collapse well">{{$question->description}}</p>
+                            </div>
+                          </div>
+                          <br><br>
+                          <textarea class="form-control" rows="2" type='text' name='{{$question->question}}'<?=$disable?>></textarea>
+                        @endif
                      
-                      }
-                          echo "<input type='submit' value='verzenden'>";
-                          echo "</form>";
-                      }?>
+                      @endforeach
+                      <input type='submit' value='verzenden'>
+                    </form>
+                  @endforeach
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+function h(e) {
+  $(e).css({'height':'auto','overflow-y':'hidden'}).height(e.scrollHeight);
+}
+$('textarea').each(function () {
+  h(this);
+}).on('input', function () {
+  h(this);
+});
+
+
+$('.row .btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $collapse = $this.closest('.collapse-group').find('.collapse');
+    $collapse.collapse('toggle');
+});
+</script>
 @endsection
